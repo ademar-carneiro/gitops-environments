2eb94a55cc050834e2373393805e82a00c23f375
quarks-ecosystem <52676720+quarks-ecosystem@users.noreply.github.com>
2022-03-15 15:50:49 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 terraform {
   backend "gcs" {
-    bucket  = "tf-state-gitops-terraform"
+    bucket  = "tf-state-gitops-terraform-1"
     prefix  = "gitops-terraform"
   }
 }
